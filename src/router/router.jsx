098cdba976affd83ff491add7606bfba72d701f4
Mysterio-O <skrabbi.019@gmail.com>
2025-06-05 16:48:16 +0600

    create browser router with react router and created a simple starting file structure

---
+++ src/router/router.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router';
+import RootLayout from '../RootLayout/RootLayout';
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        Component:RootLayout
+    }
+])
+
+export default router;
